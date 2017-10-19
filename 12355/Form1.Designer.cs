b975d5b678db0a7588377d69bfcfd11923c2f8a2
fydster <365069427@qq.com>
2017-10-19 10:13:22 +0800

    首次上传

---
+++ 12355/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace _12355
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel3.BackColor = System.Drawing.Color.Transparent;
+            this.panel3.Controls.Add(this.label9);
+            this.panel3.Controls.Add(this.label8);
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Location = new System.Drawing.Point(12, 31);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(947, 430);
+            this.panel3.TabIndex = 0;
+            this.panel3.Click += new System.EventHandler(this.ShowSet);
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("宋体", 150F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(518, 158);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(288, 185);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "00";
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("宋体", 150F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(386, 158);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(130, 185);
+            this.label8.TabIndex = 1;
+            this.label8.Text = ":";
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("宋体", 150F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(153, 160);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(289, 204);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "00";
+            // 
+            // label12
+            // 
+            this.label12.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.Transparent;
+            this.label12.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label12.ForeColor = System.Drawing.Color.White;
+            this.label12.Location = new System.Drawing.Point(25, 694);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(54, 21);
+            this.label12.TabIndex = 5;
+            this.label12.Text = "退出";
+            this.label12.Click += new System.EventHandler(this.label12_Click_1);
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(111, 15);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(39, 18);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "2分钟";
+            this.label10.Click += new System.EventHandler(this.label10_Click_1);
+            // 
+            // label11
+            // 
+            this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.ForeColor = System.Drawing.Color.White;
+            this.label11.Location = new System.Drawing.Point(161, 15);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(39, 18);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "3分钟";
+            this.label11.Click += new System.EventHandler(this.label11_Click_1);
+            // 
+            // label13
+            // 
+            this.label13.BackColor = System.Drawing.Color.Transparent;
+            this.label13.ForeColor = System.Drawing.Color.White;
+            this.label13.Location = new System.Drawing.Point(261, 15);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(39, 18);
+            this.label13.TabIndex = 17;
+            this.label13.Text = "10分钟";
+            this.label13.Click += new System.EventHandler(this.label13_Click);
+            // 
+            // label14
+            // 
+            this.label14.BackColor = System.Drawing.Color.Transparent;
+            this.label14.ForeColor = System.Drawing.Color.White;
+            this.label14.Location = new System.Drawing.Point(211, 15);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(39, 18);
+            this.label14.TabIndex = 16;
+            this.label14.Text = "5分钟";
+            this.label14.Click += new System.EventHandler(this.label14_Click);
+            // 
+            // label15
+            // 
+            this.label15.BackColor = System.Drawing.Color.Transparent;
+            this.label15.ForeColor = System.Drawing.Color.White;
+            this.label15.Location = new System.Drawing.Point(311, 15);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(39, 18);
+            this.label15.TabIndex = 18;
+            this.label15.Text = "60分钟";
+            this.label15.Click += new System.EventHandler(this.label15_Click);
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.BackColor = System.Drawing.Color.Transparent;
+            this.label16.ForeColor = System.Drawing.Color.White;
+            this.label16.Location = new System.Drawing.Point(72, 16);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(29, 12);
+            this.label16.TabIndex = 19;
+            this.label16.Text = "结束";
+            this.label16.Click += new System.EventHandler(this.label16_Click);
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.BackColor = System.Drawing.Color.Transparent;
+            this.label17.ForeColor = System.Drawing.Color.White;
+            this.label17.Location = new System.Drawing.Point(27, 16);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(29, 12);
+            this.label17.TabIndex = 20;
+            this.label17.Text = "开始";
+            this.label17.Click += new System.EventHandler(this.label17_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(971, 724);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.panel3);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+    }
+}
+
---
+++ 12355/Form1.cs
@@ -0,0 +1,412 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Media;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace _12355
+{  
+    public partial class Form1 : Form
+    {
+        /*
+        private const int SW_HIDE = 0;  //隐藏任务栏
+        private const int SW_RESTORE = 9;//显示任务栏
+        [DllImport("user32.dll")]
+        public static extern int FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll")]
+        public static extern int ShowWindow(int hwnd, int nCmdShow);
+        */
+
+        System.Timers.Timer t = new System.Timers.Timer(1000);   //实例化Timer类，设置间隔时间为10000毫秒；   
+        public int second = 0;
+        public int tempSecond = 0;
+        public int MinuteNum = 0;
+        public int SecondNum = 0;
+        public bool isStart = false;
+        public bool isFirst = true;
+        public int fenzh = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DJS(object source, System.Timers.ElapsedEventArgs e)
+        {
+            this.Invoke((EventHandler)delegate
+            {
+                tempSecond++;
+
+                int Minu = Convert.ToInt16((second - tempSecond) / 60);
+                int Secos = (second - tempSecond) % 60;
+                string MinuteS = Minu.ToString();
+                if (MinuteS.Length == 1)
+                {
+                    MinuteS = "0" + MinuteS;
+                }
+                string SecondS = Secos.ToString();
+                if (SecondS.Length == 1)
+                {
+                    SecondS = "0" + SecondS;
+                }
+
+                if (second >= tempSecond)
+                {
+                    label7.Text = MinuteS;
+                    label7.Refresh();
+
+                    label9.Text = SecondS;
+                    label9.Refresh();
+                }
+
+                if ((second - tempSecond) == 60)
+                {
+                    SoundPlayer player = new SoundPlayer();
+                    player.SoundLocation = "60.wav";
+                    player.Load();
+                    player.Play();
+                }
+                if ((second - tempSecond) <= 5)
+                {
+                    SoundPlayer player = new SoundPlayer();
+                    player.SoundLocation = "5.wav";
+                    player.Load();
+                    player.Play();
+                }
+                if (second == (tempSecond-1))
+                {
+                    SoundPlayer player = new SoundPlayer();
+                    player.SoundLocation = "0.wav";
+                    player.Load();
+                    player.Play();
+                    t.Stop();
+                    label17.Text = "开始";
+                    isStart = false;
+                }
+            });
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void ShowSet(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.BackgroundImage = Image.FromFile("bg.jpg");
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+            panel3.BackColor = System.Drawing.Color.FromArgb(255, 199, 10, 38);
+            label16.Enabled = true;
+
+            //Hide();
+
+            panel3.Hide();
+            label7.Hide();
+            label8.Hide();
+            label9.Hide();
+        }
+
+        /*
+        public static void Hide()
+        {
+            ShowWindow(FindWindow("Shell_TrayWnd", null), SW_HIDE);
+        }
+
+        public static void Show()
+        {
+            ShowWindow(FindWindow("Shell_TrayWnd", null), SW_RESTORE);
+        }
+        */
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void SetAll()
+        {
+            panel3.Show();
+            label7.Show();
+            label8.Show();
+            label9.Show();
+            if (fenzh == 60)
+            {
+                MinuteNum = 60;
+                SecondNum = 0;
+            }
+            if (fenzh == 10)
+            {
+                MinuteNum = 10;
+                SecondNum = 0;
+            }
+            if (fenzh == 5)
+            {
+                MinuteNum = 5;
+                SecondNum = 0;
+            }
+            if (fenzh == 3)
+            {
+                MinuteNum = 3;
+                SecondNum = 0;
+            }
+            if (fenzh == 2)
+            {
+                MinuteNum = 2;
+                SecondNum = 0;
+            }
+            if (fenzh == 1)
+            {
+                MinuteNum = 1;
+                SecondNum = 0;
+            }
+
+            second = MinuteNum * 60;
+
+            string MinuteS = MinuteNum.ToString();
+            if (MinuteS.Length == 1)
+            {
+                MinuteS = "0" + MinuteS;
+            }
+            if (second > 0)
+            {
+                label7.Text = MinuteS;
+                label9.Text = "00";
+                tempSecond = 0;
+                label17.Text = "开始";
+                label16.Enabled = true;
+                t.Stop();
+            }
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+            label16.Show();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void StartD(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+            if (second > 0)
+            {
+                if (isStart)
+                {
+                    isStart = false;
+                    label17.Text = "开始";
+                    t.Stop();
+                }
+                else
+                {
+                    label16.Enabled = false;
+                    isStart = true;
+                    label17.Text = "暂停";
+                    if (isFirst)
+                    {
+                        isFirst = false;
+                        t.Elapsed += new System.Timers.ElapsedEventHandler(DJS); //到达时间的时候执行事件；   
+                        t.Enabled = true;     //是否执行System.Timers.Timer.Elapsed事件；   
+                    }
+                    t.Start();
+                }
+            }
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label12_Click_1(object sender, EventArgs e)
+        {
+            Show();
+            System.Environment.Exit(0); 
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            fenzh = 1;
+            SetAll();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            fenzh = 2;
+            SetAll();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            fenzh = 3;
+            SetAll();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            fenzh = 5;
+            SetAll();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            fenzh = 10;
+            SetAll();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            fenzh = 60;
+            SetAll();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            label10.Hide();
+            label11.Hide();
+            label15.Hide();
+            label13.Hide();
+            label14.Hide();
+
+            if (second > 0)
+            {
+                if (isStart)
+                {
+                    isStart = false;
+                    label17.Text = "开始";
+                    t.Stop();
+                }
+                else
+                {
+                    isStart = true;
+                    label17.Text = "暂停";
+                    if (isFirst)
+                    {
+                        isFirst = false;
+                        t.Elapsed += new System.Timers.ElapsedEventHandler(DJS); //到达时间的时候执行事件；   
+                        t.Enabled = true;     //是否执行System.Timers.Timer.Elapsed事件；   
+                    }
+                    t.Start();
+                }
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            label10.Show();
+            label11.Show();
+            label15.Show();
+            label13.Show();
+            label14.Show();
+            panel3.Hide();
+            label7.Hide();
+            label8.Hide();
+            label9.Hide();
+            label7.Text = "00";
+            label9.Text = "00";
+            tempSecond = 0;
+            label17.Text = "开始";
+            t.Stop();
+        }
+
+        private void label10_Click_1(object sender, EventArgs e)
+        {
+            fenzh = 2;
+            SetAll();
+        }
+
+        private void label11_Click_1(object sender, EventArgs e)
+        {
+            fenzh = 3;
+            SetAll();
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+            fenzh = 5;
+            SetAll();
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+            fenzh = 10;
+            SetAll();
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+            fenzh = 60;
+            SetAll();
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+            label10.Show();
+            label11.Show();
+            label15.Show();
+            label13.Show();
+            label14.Show();
+            panel3.Hide();
+            label7.Hide();
+            label8.Hide();
+            label9.Hide();
+            label7.Text = "00";
+            label9.Text = "00";
+            tempSecond = 0;
+            label17.Text = "开始";
+            t.Stop();
+        }
+
+        private void label17_Click(object sender, EventArgs e)
+        {
+            label10.Hide();
+            label11.Hide();
+            label15.Hide();
+            label13.Hide();
+            label14.Hide();
+
+            if (second > 0)
+            {
+                if (isStart)
+                {
+                    isStart = false;
+                    label17.Text = "开始";
+                    t.Stop();
+                }
+                else
+                {
+                    isStart = true;
+                    label17.Text = "暂停";
+                    if (isFirst)
+                    {
+                        isFirst = false;
+                        t.Elapsed += new System.Timers.ElapsedEventHandler(DJS); //到达时间的时候执行事件；   
+                        t.Enabled = true;     //是否执行System.Timers.Timer.Elapsed事件；   
+                    }
+                    t.Start();
+                }
+            }
+        }
+    }
+}
